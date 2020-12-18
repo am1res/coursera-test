544664a7e08a99b447bfc29c075309f9b795387d
Amir <saulebekovamir@gmail.com>
2020-12-18 15:25:57 +0600

    module 4

---
+++ mod4_solution/script.js
@@ -0,0 +1,9 @@
+let a = ["Jessy", "Hisoka", "John", "Kakashi", "Jordan", "Joshua", "Ulquiorra", "Itachi", "Killua"]
+for(let i=0;i<a.length;i++){
+	if(a[i][0]=="j" || a[i][0]=="J"){
+		console.log("Hoodbye " + a[i])
+	}
+	else{
+		console.log("Hello " + a[i])
+	}
+}
